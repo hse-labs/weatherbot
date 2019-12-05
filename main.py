928891e78945e2f8683591aea2ca836a21f8f0e4
Aleksandra Danilova <alex.l.univ@gmail.com>
2019-12-05 13:56:16 +0300

    Daily notifications

--- main.py
+++ main.py
@@ -1,7 +1,7 @@
+import os
+import re
 import telebot as tb
 import pyowm
-import os
-
 
 TELEGRAM_TOKEN = os.getenv('TELEGRAM_TOKEN')
 bot = tb.TeleBot(token=TELEGRAM_TOKEN)
@@ -20,6 +20,10 @@ def get_weather(location):
     return status, temp, wind, humidity, pressure
 
 
+def set_up_daily():
+    pass
+
+
 @bot.message_handler(commands=['start'])
 def command_start(message):
     bot.reply_to(message, 'Привет! Чтобы узнать, какая сейчас погода в твоем городе, отправь мне его название.')
@@ -30,7 +34,12 @@ def command_help(message):
     bot.reply_to(message, 'Этот бот показывает, какая сейчас погода в городе.')
 
 
-@bot.message_handler(func=lambda msg: msg.text is not None)
+@bot.message_handler(commands=['notify'])
+def command_notify(message):
+    bot.reply_to(message, '')
+
+
+@bot.message_handler(func=lambda msg: msg.content_type == 'text')
 def get_send_message(message):
     try:
         global status, temp, wind, humidity, pressure
@@ -41,8 +50,32 @@ def get_send_message(message):
                   f'Скорость ветра {wind["speed"]} м/c\nВлажность воздуха:{humidity}%\n' \
                   f'Атмосферное давление {round(pressure["press"] / 1.333)} мм рт.ст.'
         bot.reply_to(message, text='*Прогноз погоды на сегодня:\n*' + to_send, parse_mode='Markdown')
+        msg = bot.reply_to(message, text='Установить ежедневные оповещения о погоде?')
+        bot.register_next_step_handler(msg, notification_handler)
     except Exception as e:
-        print(e)
+        bot.reply_to(message, text='Кажется, что-то пошло не так, попробуй еще раз.')
+
+
+def notification_handler(message):
+    try:
+        answer = message.text
+        if answer.lower() == 'да':
+            msg = bot.reply_to(message, text='Хорошо. В какое время присылать оповещения? \n'
+                                             'Время должно быть описано в таком формате: 00:00')
+            bot.register_next_step_handler(msg, time_setter)
+        else:
+            bot.reply_to(message, text='Приходи еще.')
+    except Exception as e:
+        bot.reply_to(message, text='Кажется, что-то пошло не так, попробуй еще раз.')
+
+
+def time_setter(message):
+    m = message.text
+    print(m)
+    pattern = r'(?:[0-1]?[0-9]|2[1-4])\:[0-5][0-9]'
+    t = re.search(pattern, m)
+    print(t)
+
 
 
 bot.polling()
